1702276db931afb8c711ad4045ed76eeff19fa18
seokminkk <99874863+seokminkk@users.noreply.github.com>
2022-08-14 01:03:57 +0900

    navbar 완성

--- react/shopping-mall/src/App.js
+++ react/shopping-mall/src/App.js
@@ -1,12 +1,21 @@
 
 import './App.css';
-
-
+import { Routes, Route, } from "react-router-dom";
+import ProductAll from './page/ProductAll';
+import Login from './page/Login';
+import ProductDetail from './page/ProductDetail';
+import Navbar from './component/Navbar';
 
 function App() {
   return (
     <div>
-    
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<ProductAll/>}/>
+        <Route path='/login' element={<Login/>}/>
+        <Route path='/product/:id' element={<ProductDetail/>}/>
+
+      </Routes>
     </div>
   );
 }
---
+++ react/shopping-mall/src/page/ProductAll.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductAll = () => {
+  return (
+    <div>전체상품페이지</div>
+  )
+}
+
+export default ProductAll
